084fd85ded6ea64959a37617dde457a133d3ec49
shirisha <Shirisha1318@gmail.com>
2020-01-09 08:12:17 +0530

    Get Square unique

--- src/PlayBoard.js
+++ src/PlayBoard.js
@@ -2,27 +2,42 @@ import React from 'react';
 import './PlayBoard.css';
 
 class PlayBoard extends React.Component {
+
+
+    getSquare(i) {
+        return (
+          <Square
+            value="X" 
+          />
+        );
+      }
     render() {
         return(
             <div>            
                 <div className="row">
-                    <button className="square">X</button>
-                    <button className="square">X</button>
-                    <button className="square">X</button>
+                    {this.getSquare(0)}
+                    {this.getSquare(1)}
+                    {this.getSquare(2)}
                 </div>
                 <div className="row">
-                    <button className="square">X</button>
-                    <button className="square">X</button>
-                    <button className="square">X</button>
+                    {this.getSquare(3)}
+                    {this.getSquare(4)}
+                    {this.getSquare(5)}
                 </div>
                 <div className="row">
-                    <button className="square">X</button>
-                    <button className="square">X</button>
-                    <button className="square">X</button>
+                    {this.getSquare(6)}
+                    {this.getSquare(7)}
+                    {this.getSquare(8)}
                 </div>  
             </div>
         )
     }
 }
-
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );  
+  }
 export default PlayBoard;
