037e3c6a18718cd25a8aef24a2ed4a2b7f07a465
shirisha <Shirisha1318@gmail.com>
2020-01-09 07:40:40 +0530

    create play area

---
+++ src/PlayBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PlayBoard.css';
+
+class PlayBoard extends React.Component {
+    render() {
+        return(
+          <div>            
+           <p>Get Play Board</p>  
+          </div>
+        )
+    }
+}
+
+export default PlayBoard;
